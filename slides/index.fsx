dbbbc0bb10f3b1cc200c38f41d86804b796cd6ba
Paulmichael Blasucci <pblasucci@gmail.com>
2015-07-28 00:46:54 -0400

    initial creation

---
+++ slides/index.fsx
@@ -0,0 +1,28 @@
+﻿(**
+- title : Level Up Your Library Development
+- description : Use Project Scaffold to go from zero to professional-grade library in a matter of minutes.
+- author : Paulmichael Blasucci
+- theme : Blood
+- transition : None
+
+***
+
+<hr/>
+
+### Level Up Your
+### Library Development
+
+<hr/>
+
+---
+
+### Paulmichael Blasucci
+
+Senior Software Engineer
+
+![QuickenLoans](images/QL.png)
+
+github.com [/pblasucci](http://github.com/pblasucci)
+
+twitter.com [/pblasucci](http://twitter.com/pblasucci)
+*)
